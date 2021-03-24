4015e40ed853abbfe1702a3cf1ed73d598ffdb29
Myra✘ <57118136+Tashaun272@users.noreply.github.com>
2021-03-24 08:21:37 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
 
-
-$("#start").click(function() {
-    $(".main").addClass("animated bounceOutUp")
-    setTimeout(function() {
-        $(".main").hide()
-
-        $("#numer").html("0")
-        $("#denom").html($("#msgamt").val())
-
-        $(".progress-div").addClass("animated bounceInUp")
-        $(".progress-div").show()
-
-        for(var i = 0; i < $("#msgamt").val(); i++) {
-            setTimeout(function() {
-                $.ajax({
-                    type: "POST",
-                    url: $("#webhook-url").val(),
-                    data: JSON.stringify({
-                        content: ($("#msg").val() == "" ? "Hide ur webhooks!" : $("#msg").val()),
-                        username: ($("#username").val() == "" ? null : $("#username").val()),
-                        avatar_url: ($("#avatar").val() == "" ? null : $("#avatar").val()),
-                    })
-                })
-                $("#numer").html(parseInt($("#numer").html()) + 1)
-            }, i * 500)
+$(function () {
+    $('#btn').click(function () {
+        var link = $('#link').val();
+        var username = $('#username').val();
+        var content = $('#content').val();
+        var avatar = $('#avatar').val();
+        if (link == null || link == "", content == null || content == "") {
+            alert("please fill in all the options");
+            return false;
         }
-    }, 2000)
-})
+
+        let i = 0;
+        let inteval = setInterval(function () {
+            $.post(link,"content:" ($("#msg").val() == "" ? "Hide ur webhooks!" : $("#msg").val()),
+                        "username::" ($("#username").val() == "" ? null : $("#username").val()),
+                        avatar_url: ($("#avatar").val() == "" ? null : $("#avatar").val()),
+
+    
+       }, 50)
+
+
+
+    });
+});
